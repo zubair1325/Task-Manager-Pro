6d3e31ae2e9b75dd83b9b2a1772c2c8b8af82569
zubair1325 <rahman22205101325@diu.edu.bd>
2026-03-01 22:42:56 +0600

    singup done

---
+++ lib/ui/widget/sanck_message.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void showSnackMessage(
+  BuildContext context,
+  String message, [
+  bool isError = false,
+]) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(message),
+      backgroundColor: isError ? Colors.red : null,
+    ),
+  );
+}
